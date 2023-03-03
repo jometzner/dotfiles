4621e38ab680646c057625e088990c9275993d9c
Johannes Metzner <jm@jometzner.de>
2023-03-03 17:20:52 +0100

    Remove java from path var

--- dot_dotfiles/system/path.zsh
+++ dot_dotfiles/system/path.zsh
@@ -1,5 +1,5 @@
 #!/bin/sh
 JAVA_HOME=$HOME/Java/jdk8u312-b07
-PATH=$JAVA_HOME/bin:~/go/bin:~/.local/bin:$PATH
+PATH=~/go/bin:~/.local/bin:$PATH
 HISTFILE=~/.zsh_history
 export HISTFILE PATH JAVA_HOME
