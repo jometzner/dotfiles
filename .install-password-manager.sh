d8fc281c08361b27a125208037b24bdd97e77c39
Johannes Metzner <johannes.metzner@indi.nl>
2024-03-21 21:25:51 +0100

    install pwd mgr on init

---
+++ .install-password-manager.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eufo pipefail
+# exit immediately if password-manager-binary is already in $PATH
+type ~/.local/bin/keeper >/dev/null 2>&1 && exit
+
+case "$(uname -s)" in
+Darwin)
+    # commands to install password-manager-binary on Darwin
+    ;;
+Linux)
+    # commands to install password-manager-binary on Linux
+    type pip3 >/dev/null 2>&1 || sudo dnf install -y python3-pip
+    pip3 install keepercommander
+    ;;
+*)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
+
+
