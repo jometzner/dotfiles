90816b939389e212bdefba02dc24fcc5a261f49d
Johannes Metzner <jm@jometzner.de>
2025-01-02 22:02:05 +0100

    remove own jdk

--- dot_dotfiles/system/path.zsh
+++ dot_dotfiles/system/path.zsh
@@ -1,7 +1,6 @@
 #!/bin/sh
-JAVA_HOME=$HOME/Java/jdk8u372-b07
 PATH=~/go/bin:~/.local/bin:$PATH
 HISTFILE=~/.zsh_history
-export HISTFILE PATH JAVA_HOME
+export HISTFILE PATH
 export SDKMAN_DIR="$HOME/.sdkman"
 export _ZO_EXCLUDE_DIRS=~/work/gradle2
