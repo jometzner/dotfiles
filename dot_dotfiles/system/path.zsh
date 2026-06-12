e260751340ec37b478df76b3d8ad99efe3f76e63
Johannes Metzner <jm@jometzner.de>
2026-06-12 15:42:17 +0200

    Make devcontainer work with podman socket

--- dot_dotfiles/system/path.zsh
+++ dot_dotfiles/system/path.zsh
@@ -3,3 +3,4 @@ PATH=~/go/bin:~/.local/bin:$PATH
 export PATH
 export SDKMAN_DIR="$HOME/.sdkman"
 export _ZO_EXCLUDE_DIRS=~/work/gradle2
+export DOCKER_HOST=unix:///run/user/$(id -u)/podman/podman.sock
